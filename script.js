4e3c8c65a97960c5f843a020660012099a13e34d
Muhammad Mohsin <mohsinraja4667@gmail.com>
2022-04-14 16:58:31 +0530

    updation_second

--- script.js
+++ script.js
@@ -19,3 +19,18 @@ var swiper = new Swiper(".mySwiper", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+/*=========================================================================
+            donate page
+========================================================================= */
+let donate_btn = document.getElementById("donate_btn");
+let donate_box = document.querySelector(".donate_box");
+
+donate_btn.addEventListener("click", ()=>{
+    if(donate_box.style.display == "none"){
+        donate_box.style.display = "flex";
+    }
+    else{
+        donate_box.style.display = "none";
+    }
+})
